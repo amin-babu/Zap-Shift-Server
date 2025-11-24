0fd93563274636ba98d185bcf1b3ca448d1e4c80
aminbabu12 <badrulaminbabu@gmail.com>
2025-11-24 23:25:22 +0600

    strap added

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const cors = require('cors');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 3000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const stripe = require('stripe')(process.env.STRIPE_SECRET);
 
 //middleware
 app.use(express.json());
@@ -32,18 +33,96 @@ async function run() {
 
       // to be like that -> http://localhost:3000/parcels?email=badrulaminbabu@gmail.com
       const { email } = req.query;
-      if(email){
+      if (email) {
         query.senderEmail = email;
       };
-      const cursor = parcelsCollection.find(query);
+
+      const options = { sort: { createdAt: -1 } };
+
+      const cursor = parcelsCollection.find(query, options);
       const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.get('/parcels/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await parcelsCollection.findOne(query);
       res.send(result);
     });
 
     app.post('/parcels', async (req, res) => {
       const parcel = req.body;
+      parcel.createdAt = new Date(); // parcel created time
       const result = await parcelsCollection.insertOne(parcel);
       res.send(result);
+    });
+
+    app.delete('/parcels/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await parcelsCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    // payment realted API
+    app.post('/payment-checkout-session', async (req, res) => {
+      const paymentInfo = req.body;
+      const amount = parseInt(paymentInfo.cost) * 100;
+      const session = await stripe.checkout.sessions.create({
+        line_items: [
+          {
+            price_data: {
+              currence: 'usd',
+              unit_amount: amount,
+              product_data: {
+                name: `Please pay for: ${paymentInfo.percelName}`
+              }
+            },
+            quantity: 1,
+          },
+        ],
+        mode: 'payment',
+        metadata: {
+          parcelId: paymentInfo.parcelId
+        },
+        customer_email: paymentInfo.senderEmail,
+        success_url: `${process.env.SIDE_DOMAIN}/dashboard/payment-success?session_id={CHECKOUT_SESSION_ID}`,
+        cancel_url: `${process.env.SIDE_DOMAIN}/dashboard/payment-cancelled`,
+      });
+
+      res.send({ url: session.url });
+    });
+
+    // old
+    app.post('/create-checkout-session', async (req, res) => {
+      const paymentInfo = req.body;
+      const amount = parseInt(paymentInfo.cost) * 100;
+
+      const session = await stripe.checkout.sessions.create({
+        line_items: [
+          {
+            price_data: {
+              currency: 'USD',
+              unit_amount: amount,
+              product_data: {
+                name: paymentInfo.percelName
+              }
+            },
+            quantity: 1,
+          },
+        ],
+        customer_email: paymentInfo.senderEmail,
+        mode: 'payment',
+        metadata: {
+          parcelId: paymentInfo.parcelId
+        },
+        success_url: `${process.env.SIDE_DOMAIN}/dashboard/payment-success`,
+        cancel_url: `${process.env.SIDE_DOMAIN}/dashboard/payment-cancelled`,
+      });
+
+      console.log(session);
+      res.send({ url: session.url });
     });
 
     await client.db("admin").command({ ping: 1 });
